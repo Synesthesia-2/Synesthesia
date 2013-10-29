6fc091a5591e089f8258d4e4db0450e0f94940b0
Joey Yang <joey@joeyyang.com>
2013-10-29 16:13:36 -0700

    server randomly assigns clients to one of two rooms

--- serverRewrite.js
+++ serverRewrite.js
@@ -1,96 +1,65 @@
-var express = require('express'),
-    http = require('http'),
-    crypto = require('crypto');
-
+var express = require('express');
 var app = express();
+var http = require('http');
 var server = http.createServer(app);
-var io = require('socket.io').listen(server, {
-  transports: ['websocket'],
-  log: false
+server.listen(8080);
+var io = require('socket.io').listen(server);
+var canvas = io.of('/canvas');
+var conductor = io.of('/conductor');
+var clients = io.of('/client');
+
+app.use(express.static(__dirname + "/public"));
+
+//////////////////////////////////////////
+///
+/// ROUTES
+///
+//////////////////////////////////////////
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.set('views', __dirname + '/views');
-app.set("view engine", "jade");
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-  res.render("index");
+app.get('/canvas', function (req, res) {
+  res.sendfile(__dirname + '/canvas.html');
 });
 
-var brushes = {};
-var canvases = {};
+app.get('/conductor', function (req, res) {
+  res.sendfile(__dirname + '/conductor.html');
+});
 
-io.sockets.on('connection', function(socket) {
-  socket.on('device', function(data) {
-    if (data.type === 'brush') {                  // TODO: Let user import case switching statement
-      var brush = new Brush(socket, setUniqueID());
-      brushes[brush.id] = brush;
-      socket.broadcast.emit("brushAdd", { brushId: brush.id } );
-      socket.emit("welcome", { id : brush.id });
-    } else if (data.type === 'canvas') {
-      var canvas = new Canvas(socket, setUniqueID());
-      canvases[canvas.id] = canvas;
-      socket.emit("welcome", { id : canvas.id });
-    }
+//////////////////////////////////////////
+///
+/// EVENTS
+///
+//////////////////////////////////////////
+
+//////////////////////////////////////////
+/// Canvas events
+//////////////////////////////////////////
+
+canvas.on('connection', function (canv) {
+  canv.emit("welcome","You're a canvas!");
+});
+
+//////////////////////////////////////////
+/// Conductor events
+//////////////////////////////////////////
+
+conductor.on('connection', function (cond) {
+  cond.emit("welcome","You're a conductor!");
+  cond.on('changeColor',function(data){
+    canvas.emit('changeColor', data);
+    clients.emit('changeColor', data);
   });
-  
-  socket.on('paint', function(data) {
-    socket.broadcast.emit('draw', data);
-  });
-
-});
-
-io.sockets.on('disconnect', function(socket) {
-  socket.on('device', function(data) {
-    if (data.type === 'brush') {                  // TODO: Let user import case switching statement
-      
-    } else if (data.type === 'canvas') {
-
-    }    
-  }
-  // add info to socket instead of vice cersa
 });
 
 //////////////////////////////////////////
-// constructor functions
+/// Client events
 //////////////////////////////////////////
 
-var Brush = function(socket, id, model) { // pseudoclassical instantiation, model? (is model 'options'?)
-  this.socket = socket;
-  this.id = id;
-  this.color = '#FF0000';                 // TODO: remove hardcoding?
-  this.brushSize = 1;                     // TODO: remove hardcoding?
-};
-
-var Canvas = function(socket, id) {
-  this.socket = socket;
-  this.id = id;
-};
-
-//////////////////////////////////////////
-// helper functions
-//////////////////////////////////////////
-
-var getUniqueID = function(set) {                   // TODO: is this function necessary?
-  var id = crypto.randomBytes(3).toString('hex');
-  while (id in set) {                               // for? || while (!id in set)
-    id = crypto.randomBytes(3).toString('hex');
-  }
-  return id;
-};
-
-var setUniqueID = function() {
-  return crypto.randomBytes(3).toString('hex');
-};
-
-var checkIDs = function(id, set) {                   // TODO: is this function necessary?
-  for (items in set) {
-    if (id === set[items].id) {
-      return false;
-    }
-  }
-  return true;
-};
-
-var port = process.env.PORT || 9001;
-server.listen(port);
+clients.on('connection', function (cli) {
+  var team = Math.round(Math.random()) ? "1" : "2";
+  cli.join(team);
+  cli.emit("welcome","You're a client on team " + team + "!");
+});
