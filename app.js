a8eabf78c489a80d9caf62449b4bdaa19a1090e7
George Bonner <georgebonnr@gmail.com>
2013-10-28 21:31:42 -0700

    first commit

--- app.js
+++ app.js
@@ -3,56 +3,21 @@ var express = require('express')
     , http=require('http')
     , server = http.createServer(app)
     , io = require('socket.io').listen(server)
+    , projector = require('projector.js');
 
 server.listen(8080);
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/browser', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/browser_org', function (req, res) {
-  // debugger;
-  console.log(__dirname);
-  res.sendfile(__dirname + '/public/index_org.html');
+app.get('/controller', function (req, res) {
+  res.sendfile(__dirname + '/controller.html');
 });
-
-app.get('/phone', function (req, res) {
-  res.sendfile(__dirname + '/phone.html');
-});
-
-app.get('/control', function (req, res) {
-  res.sendfile(__dirname + '/control.html');
-});
-
-io.sockets.on()
 
 io.sockets.on('connection', function (client) {
-  client.emit('from server', {hello:'from server'});
-  client.on('from client', function(data){console.log(data);});
-  client.on('from control', function(data){console.log(data);});
-
-  client.on('motion', function(data){
-    client.broadcast.emit('motion', data);
-  });
-
-  client.on('touchstart', function(data){
-      client.broadcast.emit('touchstart', data);
-  });
-  
-  client.on('touchmove', function(data){
-      client.broadcast.emit('touchmove', data);
-  });
-  
-  client.on('touchend', function(data){
-      client.broadcast.emit('touchend', data);
-  });
-  client.on('switch', function(data){
-    client.broadcast.emit('switch', data);
-  });
-  client.on('control', function(data){
-    client.broadcast.emit('control', data);
-  });
-
+  client.on('device', function(data){
+    (data.type).listeners();
 });
