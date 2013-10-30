ddca99f8428189a68ba387546a3cd5a7e929ee89
David Ryan Hall <david.ryan.hall@gmail.com>
2013-10-29 21:27:28 -0700

    Conductor front end first pass. Allows for selecting three color options. Needs socket information plugged into JS file.

--- public/javascript/conductor.js
+++ public/javascript/conductor.js
@@ -5,3 +5,61 @@ angular.module('conductorApp', [])
 .controller('conductorController', function($scope) {
 
 });
+
+$(document).ready(function() {
+  
+  var specOption, colorOptions;
+
+  $('#allOneColor').on('click touchend', function(e) {
+    e.preventDefault();
+    specOption = "allOneColor";
+    $('#optionPalette').show();
+    $('#optionBuilder h2').text("Pick a color to transmit:");
+  
+    $('.color').on('click touchend', function(e) {
+      e.preventDefault();
+      colorOptions = { color: $(this).data('color') };
+
+      // socket shit
+      
+      $('#optionPalette').hide();
+      $('#optionBuilder h2').text("Please select an option below:");
+      $('#currentBar h3').text("Currently Deployed: All One Color");
+    });
+  });
+
+  $('#allRandomColors').on('click touchend', function(e) {
+    e.preventDefault();
+    specOption = "allRandomColors";
+    colorOptions = { color: 'random' };
+    // socket shit
+    
+    $('#currentBar h3').text("Currently Deployed: All Random Colors");
+  });
+
+  $('#splitColors').on('click touchend', function(e) {
+    e.preventDefault();
+    specOption = "splitColors";
+    $('#optionPalette').show();
+    $('#optionBuilder h2').text("Pick colors to use:");
+    colorOptions = [];
+    $('.color').on('click touchend', function(e) {
+      e.preventDefault();
+      colorOptions.push($(this).data('color'));
+      $(this).css({ 'border-color': '#000'});
+    });
+
+    $('#submitOption').on('click touchend', function(e){
+      e.preventDefault();
+      colorsToSend = { color: colorOptions};
+      // socket
+      
+      $('#optionPalette').hide();
+      $('#optionBuilder h2').text("Please select an option below:");
+      $('#currentBar h3').text("Currently Deployed: Split Colors");
+    });
+
+  });
+
+
+});
