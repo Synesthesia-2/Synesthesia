02e9c4cee7eed676d4357cd7e2dfdc8259be435d
David Ryan Hall <david.ryan.hall@gmail.com>
2013-10-29 22:31:29 -0700

    Added model window to interact with users, and functions to start and stop listening to accel data

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,1 +1,47 @@
 // client.js
+
+
+$(document).ready(function() {
+  // When socket is emiting accel data, trigger initMotionListener
+
+  // WHen socket is just doing color disply, removeMotionListener
+
+  // Close a model on OK click
+  $('#modelWindow button').on('click touchend', closeModelMessage, false);
+});
+
+
+
+var initMotionListener = function() {
+  window.addEventListener('devicemotion', function(event) {
+    var aX = Math.floor(event.acceleration.x);
+    var aY = Math.floor(event.acceleration.y);
+    var aZ = Math.floor(event.acceleration.z);
+  }, false);
+};
+
+var removeMotionListener = function() {
+  window.removeEventListener('devicemotion', function(event) {
+    var aX = Math.floor(event.acceleration.x);
+    var aY = Math.floor(event.acceleration.y);
+    var aZ = Math.floor(event.acceleration.z);
+  }, false);
+};
+
+var makeModelMessage = function(headline, message) {
+  $('#modelWindow h1').text(headline);
+  $('#modelWindow p').text(message);
+  $('#modelMask').fadeIn(300, function() {
+    $('#modelWindow').fadeIn(400);
+  });
+};
+
+var closeModelMessage = function(e) {
+  e.preventDefault();
+  $('#modelWindow').fadeOut(300, function() {
+    $('#modelMask').fadeOut(200);
+    $('#modelWindow h1').text('');
+    $('#modelWindow p').text('');
+  });
+};
+
