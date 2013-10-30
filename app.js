c62be68c7be25df1d8ef08714cf7ea0b9fb78845
Joey Yang <joey@joeyyang.com>
2013-10-30 15:09:26 -0700

    fixes issue where client list does not update on conductor change type

--- app.js
+++ app.js
@@ -62,16 +62,20 @@ conductor.on('connection', function (conductor) {
   conductor.emit("welcome","You're a conductor!");
   conductor.on('changeColor',function(data){
     console.log('changecolor event');
+    var clients = io.of('/client');
     clients.emit('changeColor', data);
   });
   conductor.on('splitColors', function(data){
+    var clients = io.of('/client');    
     clients.in("1").emit('changeColor', {color: (data.color)[0]});
     clients.in("2").emit('changeColor', {color: (data.color)[1]});
   });
   conductor.on('allRandomColors', function(data){
+    var clients = io.of('/client');
     clients.emit('randomColor', {color: data.color});
   });
   conductor.on('switchPainting', function(data){
+    var clients = io.of('/client');
     state.painting = !state.painting;
     clients.emit('switchPainting', data);
   });
@@ -81,6 +85,7 @@ conductor.on('connection', function (conductor) {
 /// Client events
 //////////////////////////////////////////
 clients.on('connection', function (client) {
+  var clients = io.of('/client');
   var team = state["1"] - state["2"] >= 0 ? "2" : "1";
   client.join(team);
   state[team] += 1;
