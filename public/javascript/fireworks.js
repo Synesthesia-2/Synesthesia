96cd4c5c8bf122068047251acbbfdff345b7dedd
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-30 20:01:10 -0700

    fireworks initial commit

--- public/javascript/fireworks.js
+++ public/javascript/fireworks.js
@@ -1,23 +1,24 @@
-//canvas.js
-var canvasWrapper;
-var brushServer = io.connect('/canvas');
+
+var server = io.connect('/fireworks');
 
 $(document).ready(function() {
-  canvasWrapper = new CanvasWrapper();
   // when a new client connects to brushServer,
   // notify the canvas change below to whatever
   // you need - data should contain a unique
   // id for each brush
-  brushServer.on('newBrush', function(data) {
+  server.on('welcome', function(data) {
+    console.log("welcomed", data);
+  })
+  server.on('newBrush', function(data) {
     canvasWrapper.addView(data.brushId);
   });
 
   // brushServer sends a draw message with new accel
   // data. Takes a brush ID, and routes the
   // drawing to the appropriate canvas
-  brushServer.on('paint', function(data) {
-    console.log(data);
-    canvasWrapper.assign(data);
+  server.on('paint', function(data) {
+    console.log('Within fireworks.js ',data);
+    initialize(data);
   });
 
 });
