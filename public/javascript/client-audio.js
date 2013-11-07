19babe4a751ce6386e75a6a85351e02623ae6717
George Bonner <georgebonnr@gmail.com>
2013-11-07 02:30:46 -0800

    Add noise canceling and set threshold Audio app now listens to ambient sound level and sets threshold for events to be emitted. Analyzes peak ambient frequencies and counteracts them.

--- public/javascript/client-audio.js
+++ public/javascript/client-audio.js
@@ -10,11 +10,81 @@ if (contextClass) {
   server.send("web audio is not enabled, sorry."); // For development testing only.
 }
 
+var getPeaks = function(array) {
+  var sum = 0;
+  var freqs = {};
+  var i = array.length;
+  while(i--) {
+    var vol = array[i].volume;
+    var freq = Math.round(array[i].hz / 5) * 5;
+    if (freqs[freq]) {
+      freqs[freq].hits+=1;
+      freqs[freq].vol+=vol;
+    } else {
+      freqs[freq] = {hits:1,vol:vol};
+    }
+    sum+=vol;
+  }
+  return {
+    avg: sum/array.length,
+    freqs: freqs
+  };
+};
+
 var streamLoaded = function(stream) {
+  var calibrate = function() {
+    analyser.smoothingTimeConstant = 0.9;
+    var start = new Date().getTime();
+    var e = start+4000;
+    var results = [];
+    var analyze = function(){
+      var addFilter = function(freq) {
+        console.log("adding filter at " + freq);
+        var filter = audioContext.createBiquadFilter();
+        filter.type = filter.PEAKING;
+        filter.gain.value = data.avg-ptAvg;
+        filter.connect(analyser);
+        var lastFilter = dynamicFilters.length;
+        if (lastFilter) {
+          dynamicFilters[lastFilter-1].disconnect(analyser);
+          dynamicFilters[lastFilter-1].connect(filter);
+          dynamicFilters.push(filter);
+        } else {
+          loShelf.disconnect(analyser);
+          loShelf.connect(filter);
+          dynamicFilters.push(filter);
+        }
+      };
+      analyser.getFloatFrequencyData(FFTData);
+      var targetRange = findMaxWithIndex(FFTData);
+      var sample = {
+        volume: targetRange[1][1],
+        hz: convertToHz(targetRange)
+      };
+      results.push(sample);
+      var cur = new Date().getTime();
+      if (cur < e) {
+        setTimeout(analyze,50);
+      } else {
+        var data = getPeaks(results);
+        for (var f in data.freqs) {
+          var pt = data.freqs[f];
+          var ptAvg = pt.vol/pt.hits;
+          if (pt.hits > results.length*0.3) {
+            addFilter(f);
+          }
+        }
+        threshold = data.avg+20;
+      }
+    };
+    analyze();
+  };
+
   var process = function(){
+    analyser.smoothingTimeConstant = 0;
     setInterval(function(){
       analyser.getFloatFrequencyData(FFTData);
-      var targetRange = findMaxWithIndex(FFTData)
+      var targetRange = findMaxWithIndex(FFTData);
       var volume = targetRange[1][1];
       var hz = convertToHz(targetRange);
       // state.volume = volume;
@@ -23,8 +93,7 @@ var streamLoaded = function(stream) {
         hz: hz,
         volume: volume
       };
-      // console.log(data);
-      server.emit("audio",data);
+      if (volume > threshold) { server.emit("audio",data); }
     },60);
   };
 
@@ -36,44 +105,39 @@ var streamLoaded = function(stream) {
 
   var convertToHz = function(bucket) {
     var targetRange = bucket[1][0];
-    var lowDifference = ((bucket[1][1])-(bucket[0][1]));
-    var highDifference = ((bucket[1][1])-(bucket[2][1]));
-    var shift = (lowDifference < highDifference ? -(highDifference - lowDifference) : (lowDifference - highDifference));
+    var lowD = ((bucket[1][1])-(bucket[0][1]));
+    var highD = ((bucket[1][1])-(bucket[2][1]));
+    var shift = (lowD < highD ? -(highD - lowD) : (lowD - highD));
     var adjShift = (shift*0.5)*0.1;
     return (targetRange + adjShift) / 1024 * (44100 * 0.5);
   };
 
-  // TODO: Initalization loop gets ambient room sound and implements noise canceling
-
+  var threshold = -80;
+  var dynamicFilters = [];
   var microphone = audioContext.createMediaStreamSource(stream);
   var analyser = audioContext.createAnalyser();
-  analyser.smoothingTimeConstant = 0;
   var hiPass = audioContext.createBiquadFilter();
   hiPass.type = hiPass.HIGHPASS;
   hiPass.frequency.value = 200;
   var loPass = audioContext.createBiquadFilter();
   loPass.type = loPass.LOWPASS;
   loPass.frequency.value = 1200;
+  var loShelf = audioContext.createBiquadFilter();
+  loShelf.type = loShelf.LOWSHELF;
+  loShelf.frequency.value = 330;
+  loShelf.gain.value = -10;
   analyser.fftSize = 2048;
   analyser.minDecibels = -144;
   var FFTData = new Float32Array(analyser.frequencyBinCount);
   FFTData.indexOf = Array.prototype.indexOf;
-  analyser.getFloatFrequencyData(FFTData)
+  analyser.getFloatFrequencyData(FFTData);
   microphone.connect(hiPass);
   hiPass.connect(loPass);
-  loPass.connect(analyser);
-  process();
-  // console.log(microphone);
+  loPass.connect(loShelf);
+  loShelf.connect(analyser);
+  calibrate();
+  setTimeout(process, 6000);
 };
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 navigator.getUserMedia( {audio:true}, streamLoaded );
-
-  // var findVolume = function(array) {
-  //   var i = array.length;
-  //   var sum = 0;
-  //   while (i--) {
-  //     sum+= array[i]-analyser.minDecibels;
-  //   }
-  //   return (sum/1024);
-  // };
