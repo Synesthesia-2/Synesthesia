a3c1aace96538c4a45823128e666329bdda4b7a8
George Bonner <georgebonnr@gmail.com>
2013-11-08 12:39:53 -0800

    Create audio helper functions Created functions for efficiently creating filters and chaining filters

--- public/javascript/audioHelpers.js
+++ public/javascript/audioHelpers.js
@@ -1,34 +1,65 @@
-// audioHelpers.js
-
-var makeFilter = function(context,type,freq,q,gain) {
-  if (!context) { throw "context required"; }
-  if (!type || typeof(type) !== "string") { throw "type must be string"; }
+var makeFilter = function(context,type,freq,gain,q) {
+  if (!context) {throw new Error("context argument required");}
+  if (!type || typeof(type) !== "string") {throw new Error("type must be string");}
   var filter = context.createBiquadFilter();
   filter.type = filter[type];
   if (freq) { filter.frequency.value = freq; }
-  if (q) { filter.Q.value = q; }
   if (gain) { filter.gain.value = gain; }
+  if (q) { filter.Q.value = q; }
   return filter;
 };
 
-var addFilter = function(filter,chain) {
-  if (!chain.connect) {
-    chain.connect = function(node) {
-      if (!chain.length) {
-        throw new Error("nothing in chain to connect");
-      } else {
-        chain[chain.length-1].connect(node);
+var filterChain = function(){
+  var chain = {
+    first: null,
+    last: null,
+    output: null
+  };
+  chain.connect = function(node){
+    this.output = node;
+    if (this.last) {this.last.connect(this.output);}
+  };
+  chain.add = function(filter){
+    if (arguments.length > 1) {
+      for (var i=0; i<arguments.length; i++) {
+        this.add(arguments[i]);
       }
-    };
-  }
-  if (!Array.isArray(chain)) {
-    throw "chain must be an array";
-  }
-  if (chain.length) {
-    var last = chain[chain.length-1];
-    chain.push(filter);
-    last.connect(filter);
-  } else {
-    chain.push(filter);
-  }
+      return this;
+    }
+    if (!this.first) {
+      this.first = filter;
+      this.last = filter;
+      if (this.output) {this.last.connect(this.output);}
+    } else {
+      if (this.output) {
+        this.last.disconnect(this.output);
+        filter.connect(this.output);
+      }
+      this.last.connect(filter);
+      this.last = filter;
+    }
+    return this;
+  };
+  return chain;
 };
+
+// // HOW TO USE THESE HELPER FUNCTIONS:
+// var contextClass = window.AudioContext || window.webkitAudioContext;
+// if (contextClass) {
+//   var audioContext = new contextClass();
+// } else {
+//   throw new Error("webaudio not supported.")
+// }
+
+// var input = // Input can be an oscillator node,
+//             // live mic input, other filters, etc.
+
+// var hiPass = makeFilter(audioContext,"HIGHPASS",80);
+// var loPass = makeFilter(audioContext,"LOWPASS",1200);
+// var loShelf = makeFilter(audioContext,"LOWSHELF",330,-10);
+
+// var newChain = makeFilterChain();
+// // Output connection can be defined at any time.
+// newChain.connect(audioContext.destination);
+// newChain.addFilter(hiPass,loPass,loShelf);
+// input.connect(newChain.first);
