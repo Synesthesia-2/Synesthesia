c2aaa5fede830342a0daceb8cbf352242114541e
Ian Henderson <ianwhen@gmail.com>
2014-05-26 15:02:06 -0700

    Added filter to optiflow.js, corrected a few semicolons, and attempted to make a key function for linedance.js

--- public/javascript/linedance/linedance.js
+++ public/javascript/linedance/linedance.js
@@ -8,20 +8,23 @@ var width = Math.max(960, innerWidth), //640
     height = Math.max(500, innerHeight); //480
 
 var throttledUpdate = _.throttle(function(optiFlowData) {
-      if(optiFlowData.zones.length > 1000) {
-        circle.data(optiFlowData.zones)
+      // if(optiFlowData.zones.length > 1000) {
+        circle.data(optiFlowData.zones)//, function(d,i){return (d[i].x + "x" + d[i].y);})
           .transition()
           .duration(700)
-          .attr("cx", function(d) {return d.x * width / 640})
-          .attr("cy", function(d) {return d.y * height / 480})
-          .attr("r", function(d) { return Math.max(0.3, (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))))})
+          .attr("cx", function(d) {return d.x * width / 640;})
+          .attr("cy", function(d) {return d.y * height / 480;})
+          .attr("r", function(d) { return Math.max(0.3, (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
           .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
-          .style("stroke-opacity", 1)
-      }
+          .style("stroke-opacity", 1);
+      // }
 }, 200);
 
 
-server.on('optiFlowData', throttledUpdate);
+server.on('optiFlowData', function(data){
+  console.log(data.zones);
+  throttledUpdate(data);
+});
 
 var init = [];
 init.length = 1036;
@@ -45,9 +48,9 @@ var circle = svg.selectAll("circle")
    .attr("height", 15)
    .attr("cx", 400)
    .attr("cy", 400)
-   .attr("r", 5)
+   .attr("r", 5);
 
 var particle = function (dataPoint) {
   console.log(dataPoint);
   
-}
+};
--- public/javascript/optiflow/optiflow.js
+++ public/javascript/optiflow/optiflow.js
@@ -1,14 +1,24 @@
 var server = io.connect('/optiflow');
 var flow = new oflow.WebCamFlow();
 var printed = false;
+
+var filterOptiFlowData = function(data){
+  data.zones = _.filter(data.zones, function(flowzone, index){
+    return !(flowzone.x % 4);
+  });
+  return data;
+};
+
 var sendData = function(optiFlowData) {
+  optiFlowData = filterOptiFlowData(optiFlowData);
+  if (optiFlowData.zones.length < 252) {console.log((optiFlowData));}
   server.emit('optiFlowData', optiFlowData);
   if (!printed) {
     console.log(optiFlowData.zones.length);  
     printed = true;
     
   }
-}
+};
 
 server.on('welcome', function(data) {
   if (data.tracking) {
