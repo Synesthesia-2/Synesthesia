92451ca08c6f5255885764514ecc33c109717c2f
Ash Hoover <strixcuriosus@gmail.com>
2014-05-26 15:35:06 -0700

    Create new satellite projections with centers based on overall optical flow vector. Transition smoothly from one projection to the next.

--- public/javascript/satellite/satellite.js
+++ public/javascript/satellite/satellite.js
@@ -1,10 +1,14 @@
+var server = io.connect('/satellite');
+server.on('welcome', function (data) {
+    console.log('welcomed', data);
+  });
 
-var width = 1000,
-    height = 960;
+var width = Math.max(960, innerWidth), //640
+    height = Math.max(960, innerHeight); //480
 
 var projection = d3.geo.satellite()
     .distance(1.1)
-    .scale(5500)
+    .scale(6500)
     .rotate([76.00, -34.50, 32.12])
     .center([-2, 5])
     .tilt(25)
@@ -13,7 +17,7 @@ var projection = d3.geo.satellite()
 
 var projection2 = d3.geo.satellite()
     .distance(1.1)
-    .scale(5500)
+    .scale(6500)
     .rotate([76.00, -34.50, 32.12])
     .center([-4, 2])
     .tilt(25)
@@ -21,16 +25,37 @@ var projection2 = d3.geo.satellite()
     .precision(.1);
 
 
-var projection3 = d3.geo.satellite()
+// var projection3 = d3.geo.satellite()
+//     .distance(1.1)
+//     .scale(6500)
+//     .rotate([76.00, -34.50, 32.12])
+//     .center([2, 2])
+//     .tilt(24)
+//     .clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
+//     .precision(.1);
+
+var projection4 = d3.geo.satellite()
     .distance(1.1)
-    .scale(5500)
+    .scale(6500)
     .rotate([76.00, -34.50, 32.12])
-    .center([2, 2])
-    .tilt(24)
+    .center([-4, 2])
+    .tilt(25)
     .clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
     .precision(.1);
     
+var makeProjectionPath = function(uVect, vVect) {
+  var projection = d3.geo.satellite()
+    .distance(1.1)
+    .scale(6500)
+    .rotate([76.00, -34.50, 32.12])
+    .center([3 * uVect, 3 * vVect])
+    .tilt(25)
+    .clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6)
+    .precision(.1);
 
+  return d3.geo.path()
+    .projection(projection);
+}
 
 var graticule = d3.geo.graticule()
     .extent([[-93, 27], [-47 + 1e-6, 57 + 1e-6]])
@@ -42,8 +67,11 @@ var path = d3.geo.path()
 var path2 = d3.geo.path()
     .projection(projection2);
 
-var path3 = d3.geo.path()
-    .projection(projection3);
+// var path3 = d3.geo.path()
+//     .projection(projection3);
+
+var path4 = d3.geo.path()
+    .projection(projection4);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -52,7 +80,15 @@ var svg = d3.select("body").append("svg")
 svg.append("path")
     .datum(graticule)
     .attr("class", "graticule")
-    .attr("d", path)
+    .attr("d", path);
+
+//get oflow data
+//u, v typically [-1.5, 1.5]
+// make a new projection with updated center
+//transition
+
+
+d3.select("path")
     .transition()
     .duration(3000)
     .attr("d", path2)
@@ -61,6 +97,20 @@ svg.append("path")
     .attr("d", path)
     .transition()
     .duration(1000)
-    .attr("d", path3);
+    .attr("d", path4);
 
+
+
+var throttledUpdate = _.throttle(function(optiFlowData) {
+      // console.log('u,v',optiFlowData.u, optiFlowData.v)
+    var path = makeProjectionPath(optiFlowData.u, optiFlowData.v);
+    d3.select("path")
+    .transition()
+    .duration(1000)
+    .attr("d", path)
+
+}, 200);
+
+
+server.on('optiFlowData', throttledUpdate);
 d3.select(self.frameElement).style("height", height + "px");
