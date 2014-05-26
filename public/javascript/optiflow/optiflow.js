ee21ec1f0a12d565172400efee0f206ab88ee801
Ian Henderson <ianwhen@gmail.com>
2014-05-26 15:15:29 -0700

    Refactored to use browsers .filter method

--- public/javascript/optiflow/optiflow.js
+++ public/javascript/optiflow/optiflow.js
@@ -2,15 +2,11 @@ var server = io.connect('/optiflow');
 var flow = new oflow.WebCamFlow();
 var printed = false;
 
-var filterOptiFlowData = function(data){
-  data.zones = _.filter(data.zones, function(flowzone, index){
-    return !(flowzone.x % 4);
-  });
-  return data;
-};
-
 var sendData = function(optiFlowData) {
-  optiFlowData = filterOptiFlowData(optiFlowData);
+  optiFlowData.zones = optiFlowData.zones.filter(function(flowzone, index){
+    return (flowzone.x % 4 === 0);
+  });
+
   if (optiFlowData.zones.length < 252) {console.log((optiFlowData));}
   server.emit('optiFlowData', optiFlowData);
   if (!printed) {
