689ff7e7c2effe51e6062de487834bd6c9deeace
Ash Hoover <strixcuriosus@gmail.com>
2014-05-26 21:01:15 -0700

    Use conventional d3 update pattern with a key function for grassfield.js

--- public/javascript/grassfield/grassfield.js
+++ public/javascript/grassfield/grassfield.js
@@ -8,9 +8,14 @@ var width = Math.max(960, innerWidth), //640
     height = Math.max(500, innerHeight); //480
 
 var throttledUpdate = _.throttle(function(optiFlowData) {
-      var linedata = reformatOptiFlowZones(optiFlowData);
-      if(linedata.length > 100) {
-        line.data(linedata)
+    var linedata = reformatOptiFlowZones(optiFlowData);
+    
+    var line = svg.selectAll("line")
+       .data(linedata, function(d,i) { return (d.x + "x" + d.y)});
+
+       
+          
+        line
           .transition()
           .duration(700)
           .attr("x1", function(d) {return d.x * width / 640})
@@ -20,8 +25,17 @@ var throttledUpdate = _.throttle(function(optiFlowData) {
           .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
           .style("stroke-opacity", 1)
 
+        line.enter()
+          .append('line')
+          .transition()
+          .duration(700)
+          .attr("x1", function(d) {return d.x * width / 640})
+          .attr("y1", function(d) {return d.y * height / 480})
+          .attr("x2", function(d) {return d.xPrime * width / 640})
+          .attr("y2", function(d) {return d.yPrime * height / 480})
+          .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
+          .style("stroke-opacity", 1)
 
-      }
 }, 200);
 
 var reformatOptiFlowZones = function (optiFlowData) {
@@ -37,11 +51,6 @@ var reformatOptiFlowZones = function (optiFlowData) {
 
 server.on('optiFlowData', throttledUpdate);
 
-var init = [];
-init.length = 1036;
-
-var i = 0;
-
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -51,10 +60,7 @@ svg.append("rect")
     .attr("height", height)
     .on(("ontouchstart" in document ? "touchmove" : "mousemove"), particle);
 
-var line = svg.selectAll("line")
-   .data(init)
-   .enter()
-   .append('line')
+
    // .attr("width", 10)
    // .attr("height", 15)
    // .attr("cx", 400)
