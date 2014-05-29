bfd80944c0f8390d65d4b6c64c93656c58f83079
Ash Hoover <strixcuriosus@gmail.com>
2014-05-28 17:56:06 -0700

    Throttle optical flow data emission

--- public/javascript/optiflow/optiflow.js
+++ public/javascript/optiflow/optiflow.js
@@ -1,8 +1,49 @@
+// var server = io.connect('/optiflow');
+// var flow = new oflow.WebCamFlow();
+// var printed = false;
+// var sendData = function(optiFlowData) {
+//   server.emit('optiFlowData', optiFlowData);
+//   if (!printed) {
+//     console.log(optiFlowData.zones.length);  
+//     printed = true;
+    
+//   }
+// }
+
+// server.on('welcome', function(data) {
+//   if (data.tracking) {
+//     flow.startCapture();
+//   } else {
+//     $h1.text('Connected. Optical flow tracking off.');
+//   }
+// });
+
+// server.on('reset', function() {
+//   flow.stopCapture();
+// });
+
+// flow.onCalculated(sendData);
+
 var server = io.connect('/optiflow');
 var flow = new oflow.WebCamFlow();
+var printed = false;
+
+var sendData = function(optiFlowData) {
+  optiFlowData.zones = optiFlowData.zones.filter(function(flowzone, index){
+    return (index % 4 === 0);
+  });
+  server.emit('optiFlowData', optiFlowData);
+  if (!printed) {
+    console.log(optiFlowData.zones.length);  
+    printed = true;
+    
+  }
+};
+
+var throttledSendData = _.throttle(sendData, 200);
 
 server.on('welcome', function(data) {
-  if (data.tracking) {
+  if (true) {
     flow.startCapture();
   } else {
     $h1.text('Connected. Optical flow tracking off.');
