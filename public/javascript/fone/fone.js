047e6cd877affcdf34d550d8563224451149483f
Ian Henderson <ianwhen@gmail.com>
2014-06-06 21:52:28 -0700

    Added some comments to fone.js and removed unused code

--- public/javascript/fone/fone.js
+++ public/javascript/fone/fone.js
@@ -1,8 +1,13 @@
 var server = io.connect('/fone');
 var $h3 = $('h3');
-var currentOrientation = {};
+var currentOrientation = {}; // This object will act as a container for all of the orientation/motion data
 var isTracking = false;
 
+// On connection, the server will send this socket's sessionId back to it.
+// By attaching it to currentOrientation we can tag the motion data sent to the
+// server with a unique id. This is used in the 'spotlights' visualizer to
+// allow individual control of each circle. You might be able to use it for other
+// multi-user interactive visualizers...
 server.on('sessionId', function(data){currentOrientation.id = data; });
 
 server.on('welcome', function(data) {
@@ -17,14 +22,7 @@ server.on('reset', function() {
   stopTrack();
 });
 
-server.on('toggleMotion', function(data) {
-  if (data.motion) {
-    startTrack();
-  } else {
-    stopTrack();
-  }
-});
-
+// Right now, this is just hooked up to a simple jQuery listener in fone.jade
 var toggleTracking = function(){
   isTracking = !isTracking;
   if (isTracking) {
@@ -59,15 +57,19 @@ var onDeviceOrientation = function(event) {
   currentOrientation.beta = Math.floor(event.beta);
   currentOrientation.gamma = Math.floor(event.gamma);
 
-  // For testing:
+  // For testing. Uncomment these to display the phones orientation data on it
   // $('#alpha').text("Alpha: " + (currentOrientation.alpha));
   // $('#beta').text("Beta: " + (currentOrientation.beta));
   // $('#gamma').text("Gamma: " + (currentOrientation.gamma));
 };
 
+// Notice that 'onDeviceOrientation' is constantly updating currentOrientation
+// as the device rotates but 'onDeviceMotion' does the actual emitting, along
+// with the motion data.
 var onDeviceMotion = function(event) {
   var accel = event.acceleration;
-  var totalAcc = Math.floor(Math.abs(accel.x) + Math.abs(accel.y) + Math.abs(accel.z));
+  // Find magnitude of accelertation vector
+  var totalAcc = Math.sqrt(Math.pow(accel.x, 2) + Math.pow(accel.y, 2) + Math.pow(accel.z, 2));
   currentOrientation.accel = accel;
   currentOrientation.totalAcc = totalAcc;
   server.emit('audienceMotionData', currentOrientation);
