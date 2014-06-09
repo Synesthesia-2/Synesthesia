b0fb76323815e95a77b75b5a0e7e1dd7e410f16f
Hack Reactor Students <accounts@hackreactor.com>
2014-06-08 17:52:00 -0700

    Implemented whitebar visualizer

--- public/javascript/visualizers/whitebar/whitebar.js
+++ public/javascript/visualizers/whitebar/whitebar.js
@@ -8,96 +8,43 @@ jQuery(function($) {
   var WIDTH = innerWidth;
   var HEIGHT = innerHeight;
 
-  // Increase this to make shakes count for less. Default is 1.
-  var DIFFICULTY = 0.2; 
+  // Increase this to make shakes count for more.
+  var SENSITIVITY = 35;
 
+  // This is the minimum value that the meter will contract to, even without any motion. (written as a decimal percentage)
+  var MINIMUMHEIGHT = 0.05;
+  var minH = MINIMUMHEIGHT * HEIGHT;
 
   // This will hold the sum of all incoming acceleration data
   var shakeData = 0;
-  // This will hold the current 'score', which is just a snapshot of shakeData
-  var currentScore = 0;
-  // This tracks the highest 'score' so far
-  var hiScore = 0;
+  // This will hold the current value, which is just a snapshot of shakeData
+  var currentValue = 0;
 
   var svg = d3.select("body").append("svg")
       .style("background-color", "black")
       .attr("width", WIDTH)
       .attr("height", HEIGHT);
     svg.append("rect");
-    svg.append("text").attr("id", "maintext");
-    svg.append("text").attr("id", "score");
 
-  // This function smoothes out the jitteriness of transitioning between very 
+  // This function smoothes out the jitteriness of transitioning between very
   // different scores. Set 'z' higher for more smoothing, and lower for less effect
   var zFilter = function(inputData, previousValue){
     var z = 0.9; // set this between 0 and 1
     return (z*previousValue + (1-z)*inputData);
   };
 
-  var foneVisualize = function(score){
+  var foneVisualize = function(value){
     var bars = d3.select("rect");
-    var text = d3.select("#maintext");
-    var scoretext = d3.select("#score");
-
-    // Insert your favorite shake-messages here. The key corresponds to 
-    // how much of the shakometer is filled.
-    var labels = {
-      0: "So weak.",
-      1: "Do you even shake bro?",
-      2: "Warm up then come back.",
-      3: "D+",
-      4: "Okay, not bad.",
-      5: "Legit shaker!",
-      6: "Crushing it!",
-      7: "Beast!!",
-      8: "DANGER: Approching shakebuffer limit!!",
-      9: "DANGER: Way too many shakes!!!",
-      10: "SHAKE OVERLOAD!SHAKE OVERLOAD!SHAKE OVERLOAD!"
-    };
-
-    // set text to be displayed
-    var displayText = (score >= HEIGHT) ? "MAX SHAKES!!!!" : score + " shakes";
-
-    // This is for selecting the 'smacktalk' to be displayed
-    var percent = Math.floor(10 * score/HEIGHT);
-    var maxlabel = Math.max(Object.keys(labels));
-    var subText;
-    if (percent > maxlabel){
-      subText = labels[maxlabel];
-    } else {
-      subText = labels[percent] || "";
-    }
+    var barHeight = minH + value;
 
     bars
       .transition()
       .duration(100)
       .attr("x", 0)
-      .attr("y", HEIGHT - score)
-      .attr("height", score)
+      .attr("y", HEIGHT - barHeight)
+      .attr("height", barHeight)
       .attr("width", WIDTH)
-      .style("fill", function(d,i){return "hsl(" + (score/HEIGHT*360) + ",100%,50%)";});
-
-    text
-      .transition()
-      .duration(100)
-      .attr("x", WIDTH / 2)
-      .attr("y", HEIGHT / 2)
-      .text(subText + " " + displayText)
-      .attr("font-size", "48px")
-      .attr("font-family", "sans-serif")
-      .attr("text-anchor", "middle")
-      .attr("fill", "white");
-
-    scoretext
-      .transition()
-      .duration(100)
-      .attr("x", WIDTH * 0.75)
-      .attr("y", HEIGHT * 0.25)
-      .text("HiScore: " + hiScore)
-      .attr("font-size", "48px")
-      .attr("font-family", "sans-serif")
-      .attr("text-anchor", "middle")
-      .attr("fill", "white");
+      .style("fill", "white");
   };
 
   server.on('audienceMotionData', function(data){
@@ -105,11 +52,10 @@ jQuery(function($) {
   });
 
   setInterval(function(){
-    shakeData = Math.floor(shakeData/ DIFFICULTY);
-    currentScore = Math.floor(zFilter(shakeData, currentScore)); 
-    hiScore = (currentScore > hiScore) ? currentScore : hiScore;
-    foneVisualize(currentScore);
+    shakeData = Math.floor(shakeData * SENSITIVITY);
+    currentValue = Math.floor(zFilter(shakeData, currentValue));
+    foneVisualize(currentValue);
     // Reset shakeData to 0, otherwise meter will keep growing
     shakeData = 0;
-  }, 100);
+  }, 50);
 });
